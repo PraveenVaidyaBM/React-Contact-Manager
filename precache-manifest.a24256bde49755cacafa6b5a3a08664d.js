d0b51dd493fed3b792572d1c8ade2f4f7a3b5e91
Praveen B M <49068007+PraveenVBM@users.noreply.github.com>
2020-09-30 00:10:04 +0530

    Updates

---
+++ precache-manifest.a24256bde49755cacafa6b5a3a08664d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d66372a40f44217f4f4176a954200cef",
+    "url": "/contact-manager-app/index.html"
+  },
+  {
+    "revision": "37e5cce45dd860d7730e",
+    "url": "/contact-manager-app/static/css/2.dc17f35a.chunk.css"
+  },
+  {
+    "revision": "37e5cce45dd860d7730e",
+    "url": "/contact-manager-app/static/js/2.786ee40b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contact-manager-app/static/js/2.786ee40b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a586e013f163456dbbb",
+    "url": "/contact-manager-app/static/js/main.dc809a72.chunk.js"
+  },
+  {
+    "revision": "b09221d9173911eb0e28",
+    "url": "/contact-manager-app/static/js/runtime-main.73adea87.js"
+  }
+]);
